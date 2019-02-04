4b2662146a27763421467816e4acb45468a244ff
GarrettMatthews <garrettematthews@gmail.com>
2019-02-04 12:53:03 -0700

    more work

--- mosaicv01.py
+++ mosaicv01.py
@@ -585,5 +585,14 @@ def mosaic():
             t.left(90)
         t.end_fill()
         t.forward(10)
-            
+    t.right(90)
+    t.forward(10)
+    t.right(90)
+    for i in range(27):
+        t.begin_fill()
+        for z in range(4):
+            t.forward(10)
+            t.right(90)
+        t.end_fill()
+        t.forward(10)
     
